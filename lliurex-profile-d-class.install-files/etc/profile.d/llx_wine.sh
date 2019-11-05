3c9f2fbf420d00263e6fe837d8b2b4987dbee880
cless <hectorgh@gmail.com>
2019-11-05 14:36:37 +0100

    trying to fix wine path

--- lliurex-profile-d-class.install-files/etc/profile.d/llx_wine.sh
+++ lliurex-profile-d-class.install-files/etc/profile.d/llx_wine.sh
@@ -3,8 +3,12 @@
 PATH_TO_WINE_PREFIX=""
 NAMEDIR="Documents"
 
-
-PATH_TO_WINE_PREFIX="$(xdg_translator "$NAMEDIR")"
+if [ -e "$HOME/.config/user-dirs.created" ]; then
+	. "$HOME/.config/user-dirs.created"
+	PATH_TO_WINE_PREFIX=$TOKEN_DOCUMENTS
+else
+	PATH_TO_WINE_PREFIX="$(xdg_translator "$NAMEDIR")"
+fi
 
 if [ -z $PATH_TO_WINE_PREFIX ] ; then
 	PATH_TO_WINE_PREFIX=$NAMEDIR
